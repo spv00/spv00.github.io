800ff555f3439b7b82bbc76158af4358a103224e
spv00 <sean.pa.voss@gmail.com>
2022-05-07 19:00:16 +0200

    istfg

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -35,8 +35,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1836aec0f692427f9d4488d7aac1a4b2",
-"/": "1836aec0f692427f9d4488d7aac1a4b2",
+"index.html": "46ae41143076d8f18618c368d8194a15",
+"/": "46ae41143076d8f18618c368d8194a15",
 "main.dart.js": "508b7920b736fcad43f71996da1fd721",
 "manifest.json": "0cc78bc851fa4bb23c1defdafd9b332f",
 "version.json": "c4f484197ca0a89ceae05d09d6f3f3a1"
